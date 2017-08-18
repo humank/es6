07ee739c16650b91f181d6ac6674df053dc93d08
kimkao <yikaikao@gmail.com>
2017-08-18 12:45:01 +0800

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+
+    entry: ['./app/index.js'],
+    output: {
+        path: path.resolve(__dirname,'./build'),
+        filename: 'bundle.js'
+    },
+    module:{
+        loaders:{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }
+    },
+    devServer:{
+        port: 9000,
+        contentBase: './build',
+        inline: true
+    }
+}
